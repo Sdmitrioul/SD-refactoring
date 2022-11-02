798897a91c14312c2422579dea549d31ce6e34c9
”Dmitri <dmitri.zd1958@gmail.com>
2022-11-02 18:39:35 +0100

    add tests for query servlet + remove unessary imports

--- src/test/java/ru/akirakozov/sd/refactoring/GetProductServletTest.java
+++ src/test/java/ru/akirakozov/sd/refactoring/GetProductServletTest.java
@@ -8,7 +8,6 @@ import ru.akirakozov.sd.refactoring.util.Pair;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
--- src/test/java/ru/akirakozov/sd/refactoring/QueryServletTest.java
+++ src/test/java/ru/akirakozov/sd/refactoring/QueryServletTest.java
@@ -3,8 +3,16 @@ package ru.akirakozov.sd.refactoring;
 import org.junit.Test;
 import ru.akirakozov.sd.refactoring.base.TestBase;
 import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
+import ru.akirakozov.sd.refactoring.util.Pair;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class QueryServletTest extends TestBase {
     public QueryServletTest() {
@@ -12,17 +20,58 @@ public class QueryServletTest extends TestBase {
     }
     
     @Test
-    public void testMinProduct() {
+    public void testMinProduct() throws SQLException, ServletException, IOException {
+        final List<Pair<String, Integer>> products = getProducts();
     
+        insertProduct(products);
+    
+        when(request.getParameter("command")).thenReturn("min");
+        when(request.getMethod()).thenReturn("GET");
+    
+        servlet.service(request, response);
+    
+        assertEquals(htmlResult("<h1>Product with min price: </h1>\nairpods\t10</br>\n"), writer.toString());
     }
     
     @Test
-    public void testMaxProduct() {
+    public void testMaxProduct() throws SQLException, ServletException, IOException {
+        final List<Pair<String, Integer>> products = getProducts();
     
+        insertProduct(products);
+    
+        when(request.getParameter("command")).thenReturn("max");
+        when(request.getMethod()).thenReturn("GET");
+    
+        servlet.service(request, response);
+        
+        assertEquals(htmlResult("<h1>Product with max price: </h1>\nmac\t1000</br>\n"), writer.toString());
     }
     
     @Test
-    public void testSumProduct() {
+    public void testSumProduct() throws SQLException, ServletException, IOException {
+        final List<Pair<String, Integer>> products = getProducts();
     
+        insertProduct(products);
+    
+        when(request.getParameter("command")).thenReturn("sum");
+        when(request.getMethod()).thenReturn("GET");
+    
+        servlet.service(request, response);
+        
+        assertEquals(htmlResult("Summary price: \n1160\n"), writer.toString());
+    }
+    
+    @Test
+    public void testCountProduct() throws SQLException, ServletException, IOException {
+        final List<Pair<String, Integer>> products = getProducts();
+    
+        insertProduct(products);
+    
+        when(request.getParameter("command")).thenReturn("count");
+        when(request.getMethod()).thenReturn("GET");
+    
+        servlet.service(request, response);
+    
+        assertEquals(htmlResult("Number of products: \n4\n"), writer.toString());
     }
 }
