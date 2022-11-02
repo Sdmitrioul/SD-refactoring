d873a1e8a1221253b7c3dd5b83a93140e80e9b1b
”Dmitri <dmitri.zd1958@gmail.com>
2022-11-02 18:44:19 +0100

    add test on unknown command

--- src/test/java/ru/akirakozov/sd/refactoring/QueryServletTest.java
+++ src/test/java/ru/akirakozov/sd/refactoring/QueryServletTest.java
@@ -6,7 +6,6 @@ import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
 import ru.akirakozov.sd.refactoring.util.Pair;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -22,26 +21,26 @@ public class QueryServletTest extends TestBase {
     @Test
     public void testMinProduct() throws SQLException, ServletException, IOException {
         final List<Pair<String, Integer>> products = getProducts();
-    
+        
         insertProduct(products);
-    
+        
         when(request.getParameter("command")).thenReturn("min");
         when(request.getMethod()).thenReturn("GET");
-    
+        
         servlet.service(request, response);
-    
+        
         assertEquals(htmlResult("<h1>Product with min price: </h1>\nairpods\t10</br>\n"), writer.toString());
     }
     
     @Test
     public void testMaxProduct() throws SQLException, ServletException, IOException {
         final List<Pair<String, Integer>> products = getProducts();
-    
+        
         insertProduct(products);
-    
+        
         when(request.getParameter("command")).thenReturn("max");
         when(request.getMethod()).thenReturn("GET");
-    
+        
         servlet.service(request, response);
         
         assertEquals(htmlResult("<h1>Product with max price: </h1>\nmac\t1000</br>\n"), writer.toString());
@@ -50,12 +49,12 @@ public class QueryServletTest extends TestBase {
     @Test
     public void testSumProduct() throws SQLException, ServletException, IOException {
         final List<Pair<String, Integer>> products = getProducts();
-    
+        
         insertProduct(products);
-    
+        
         when(request.getParameter("command")).thenReturn("sum");
         when(request.getMethod()).thenReturn("GET");
-    
+        
         servlet.service(request, response);
         
         assertEquals(htmlResult("Summary price: \n1160\n"), writer.toString());
@@ -64,14 +63,25 @@ public class QueryServletTest extends TestBase {
     @Test
     public void testCountProduct() throws SQLException, ServletException, IOException {
         final List<Pair<String, Integer>> products = getProducts();
-    
+        
         insertProduct(products);
-    
+        
         when(request.getParameter("command")).thenReturn("count");
         when(request.getMethod()).thenReturn("GET");
-    
+        
         servlet.service(request, response);
-    
+        
         assertEquals(htmlResult("Number of products: \n4\n"), writer.toString());
+    }
+    
+    @Test
+    public void unknownMethodTest() throws ServletException, IOException {
+        final String command = "jwefnjs";
+        when(request.getParameter("command")).thenReturn(command);
+        when(request.getMethod()).thenReturn("GET");
+        
+        servlet.service(request, response);
+        
+        assertEquals("Unknown command: " + command + "\n", writer.toString());
     }
 }
