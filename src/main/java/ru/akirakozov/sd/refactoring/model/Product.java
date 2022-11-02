833eddc39da3e7708a92794aabb7cea353d0ad4b
”Dmitri <dmitri.zd1958@gmail.com>
2022-11-02 20:05:40 +0100

    Refactor: add product model

---
+++ src/main/java/ru/akirakozov/sd/refactoring/model/Product.java
@@ -0,0 +1,35 @@
+package ru.akirakozov.sd.refactoring.model;
+
+public final class Product {
+    private final String name;
+    private final int cost;
+    
+    public Product(final String name, final int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getCost() {
+        return cost;
+    }
+    
+    @Override
+    public String toString() {
+        return toString("(", ", ", ")");
+    }
+    
+    public String toString(String prepend, String delimiter, String append) {
+        StringBuilder result = new StringBuilder();
+        
+        result.append(prepend)
+                .append(name)
+                .append(delimiter)
+                .append(append);
+        
+        return result.toString();
+    }
+}
