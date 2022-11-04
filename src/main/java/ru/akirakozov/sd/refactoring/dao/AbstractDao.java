74348e0b242ce02464ad0ac2658f2c1cbdadc378
”Dmitri <dmitri.zd1958@gmail.com>
2022-11-04 11:18:52 +0100

    Refactor: fix modification of method

--- src/main/java/ru/akirakozov/sd/refactoring/dao/AbstractDao.java
+++ src/main/java/ru/akirakozov/sd/refactoring/dao/AbstractDao.java
@@ -15,7 +15,7 @@ public abstract class AbstractDao {
         this.dbName = dbName;
     }
     
-    protected int execute(final String sql) throws DaoException {
+    public int execute(final String sql) throws DaoException {
         try (final Connection connection = DriverManager.getConnection(dbName)) {
             try (final Statement statement = connection.createStatement()) {
                 return statement.executeUpdate(sql);
